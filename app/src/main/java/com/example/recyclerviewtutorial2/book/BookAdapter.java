ec794b7f28dcbf16dfe5e015ace9ac63754ba28c
danglvh1004 <lvhd.hdlv@gmail.com>
2021-01-24 16:36:01 +0700

    Commit

---
+++ app/src/main/java/com/example/recyclerviewtutorial2/book/BookAdapter.java
@@ -0,0 +1,70 @@
+package com.example.recyclerviewtutorial2.book;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerviewtutorial2.R;
+
+import java.util.List;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.BookViewHolder> {
+
+    private List<Book> mBooks;
+    private Context mContext;
+
+    public BookAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public void setData(List<Book> list) {
+        mBooks = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_book, parent, false);
+        return new BookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
+        Book book = mBooks.get(position);
+        if (book == null) {
+            return;
+        } else {
+            holder.imgBook.setImageResource(book.getResourceId());
+            holder.tvTitle.setText(book.getTitle());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+
+        if (mBooks != null) {
+            return mBooks.size();
+        }
+        return 0;
+    }
+
+    class BookViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imgBook;
+        TextView tvTitle;
+
+        public BookViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imgBook = itemView.findViewById(R.id.img_book);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+        }
+    }
+}
